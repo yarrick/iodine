37480562401b30027e4e6db6e4898d0b0a1b0f63
Erik Ekman <yarrick@kryo.se>
2007-12-01 22:10:48 +0000

    Added packet_init

--- src/user.c
+++ src/user.c
@@ -46,9 +46,8 @@ init_users(in_addr_t my_ip)
 		users[i].id = i;
 		snprintf(newip, sizeof(newip), "0.0.0.%d", i + 1);
 		users[i].tun_ip = my_ip + inet_addr(newip);;
-		users[i].inpacket.len = 0;
-		users[i].inpacket.offset = 0;
-		users[i].outpacket.len = 0;
+		packet_init(&(users[i].inpacket));
+		packet_init(&(users[i].outpacket));
 		users[i].q.id = 0;
 	}
 }
