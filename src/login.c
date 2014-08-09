0e9d1bd328829e650b9dee7e28322cce60115796
Ryan Welton <ryan.g.welton@gmail.com>
2014-08-08 22:19:58 -0400

    Replace MD5 with library version

--- src/login.c
+++ src/login.c
@@ -25,7 +25,27 @@
 #include <arpa/inet.h>
 #endif
 
-#include "md5.h"
+#if defined( __APPLE__ )
+
+    #include <CommonCrypto/CommonDigest.h>
+
+    #ifdef MD5_DIGEST_LENGTH
+
+        #undef MD5_DIGEST_LENGTH
+
+    #endif
+
+    #define MD5_Init            CC_MD5_Init
+    #define MD5_Update          CC_MD5_Update
+    #define MD5_Final           CC_MD5_Final
+    #define MD5_DIGEST_LENGTH   CC_MD5_DIGEST_LENGTH
+    #define MD5_CTX             CC_MD5_CTX
+
+#else
+
+    #include <openssl/md5.h>
+
+#endif
 
 /*
  * Needs a 16byte array for output, and 32 bytes password
@@ -34,7 +54,6 @@ void
 login_calculate(char *buf, int buflen, const char *pass, int seed)
 {
         unsigned char temp[32];
-        md5_state_t ctx;
         int *ix;
         int i;
         int k;
@@ -51,9 +70,9 @@ login_calculate(char *buf, int buflen, const char *pass, int seed)
                 *ix++ = htonl(k);
         }
 
-        md5_init(&ctx);
-        md5_append(&ctx, temp, 32);
-        md5_finish(&ctx, (unsigned char *) buf);
-
+        MD5_CTX md5;
+        MD5_Init(&md5);
+        MD5_Update(&md5, temp, sizeof(temp));
+        MD5_Final((unsigned char *)buf, &md5);
 }
 
