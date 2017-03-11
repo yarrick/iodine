yarrick/iodine

commit 619ede5da857b9e6d3a9a84a5b6ebdb25b4d8fdc

Add options to force IP version for client DNS traffic

src/iodine.c
@@ -137,6 +137,7 @@ main(int argc, char **argv)
 	int rtable = 0;
 	struct sockaddr_storage nameservaddr;
 	int nameservaddr_len;
+	int nameserv_family;
 
 	nameserv_host = NULL;
 	topdomain = NULL;
@@ -159,6 +160,7 @@ main(int argc, char **argv)
 	lazymode = 1;
 	selecttimeout = 4;
 	hostname_maxlen = 0xFF;
+	nameserv_family = AF_UNSPEC;
 
 #ifdef WINDOWS32
 	WSAStartup(req_version, &wsa_data);
@@ -175,8 +177,14 @@ main(int argc, char **argv)
 		__progname++;
 #endif
 
-	while ((choice = getopt(argc, argv, "vfhru:t:d:R:P:m:M:F:T:O:L:I:")) != -1) {
+	while ((choice = getopt(argc, argv, "46vfhru:t:d:R:P:m:M:F:T:O:L:I:")) != -1) {
 		switch(choice) {
+		case '4':
+			nameserv_family = AF_INET;
+			break;
+		case '6':
+			nameserv_family = AF_INET6;
+			break;
 		case 'v':
 			version();
 			/* NOTREACHED */
@@ -279,7 +287,7 @@ main(int argc, char **argv)
 	}
 
 	if (nameserv_host) {
-		nameservaddr_len = get_addr(nameserv_host, DNS_PORT, AF_UNSPEC, 0, &nameservaddr);
+		nameservaddr_len = get_addr(nameserv_host, DNS_PORT, nameserv_family, 0, &nameservaddr);
 		if (nameservaddr_len < 0) {
 			errx(1, "Cannot lookup nameserver '%s': %s ",
 				nameserv_host, gai_strerror(nameservaddr_len));

README
@@ -104,11 +104,13 @@ end of the tunnel. In this case, ping 192.168.99.1 from the iodine client, and
 MISC. INFO:
 
 IPv6:
-At the moment the iodined server only supports IPv4. The client can use
-IPv4 or IPv6 nameservers to connect, and the relay nameservers will translate
-between protocols automatically if needed. If the server hostname has an AAAA
-record registered IPv6 mode will not work - in this case the client must be
-configured with an IPv4 nameserver. The data inside the tunnel is IPv4 only.
+At the moment the iodined server only supports IPv4. The data inside the tunnel
+is IPv4 only.
+
+The client can use IPv4 or IPv6 nameservers to connect to iodined. The relay
+nameservers will translate between protocols automatically if needed. Use
+options -4 or -6 to force the client to use a specific IP version for its DNS
+queries.
 
 Routing:
 It is possible to route all traffic through the DNS tunnel. To do this, first

man/iodine.8
@@ -7,7 +7,7 @@ iodine, iodined \- tunnel IPv4 over DNS
 
 .B iodine [-h]
 
-.B iodine [-f] [-r] [-u
+.B iodine [-4] [-6] [-f] [-r] [-u
 .I user
 .B ] [-P
 .I password
@@ -131,6 +131,12 @@ Apply SELinux 'context' after initialization.
 .B -F pidfile
 Create 'pidfile' and write process id in it.
 .SS Client Options:
+.TP
+.B -4
+Force IPv4 DNS queries
+.TP
+.B -6
+Force IPv6 DNS queries
 .TP
 .B -r
 Skip raw UDP mode. If not used, iodine will try getting the public IP address

commit 84ded018cf526b4256138ce0319d82b726d3f357

iodine: add usage for -4 and -6 command line arguments
Those arguments were introduced, but not documented.

Fixes: 619ede5d ("Add options to force IP version for client DNS traffic")
Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

src/iodine.c
@@ -71,7 +71,7 @@ static inline void help(bool verbose) __attribute__((noreturn));
 static void
 help(bool verbose) {
 	fprintf(stderr, "iodine IP over DNS tunneling client\n\n"
-	                "Usage: %s [-fhrv] [-u user] [-t chrootdir] [-d device] [-P password]\n"
+	                "Usage: %s [-46fhrv] [-u user] [-t chrootdir] [-d device] [-P password]\n"
 			"              [-m maxfragsize] [-M maxlen] [-T type] [-O enc] [-L 0|1] [-I sec]\n"
 			"              [-z context] [-F pidfile] [nameserver] topdomain\n", __progname);
 
@@ -79,6 +79,8 @@ help(bool verbose) {
 		exit(2);
 
 	fprintf(stderr, "\nOptions to try if connection doesn't work:\n"
+			"  -4 to connect only to IPv4\n"
+			"  -6 to connect only to IPv6\n"
 			"  -T force dns type: NULL, PRIVATE, TXT, SRV, MX, CNAME, A (default: autodetect)\n"
 			"  -O force downstream encoding for -T other than NULL: Base32, Base64, Base64u,\n"
 			"     Base128, or (only for TXT:) Raw  (default: autodetect)\n"